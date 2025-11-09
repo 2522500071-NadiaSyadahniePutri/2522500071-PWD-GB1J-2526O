a22c46aa88e88e18a5df803e985736476a1aec02
Nadia Syadahnie Putri <2522500071@mahasiswa.atmaluhur.ac.id>
2025-11-09 22:42:18 +0700

    skrip php hello world

---
+++ pertemuan-06/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Judul Halaman</title>
+      <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <header>
+            <h1>Ini Header</h1>
+            <button class="menu-toggle" id="menuToggle" aria-label="Toggle Navigation">
+                &#9776;
+            </button>
+            <nav>
+                <ul>
+                    <li><a href="#home">Beranda</a></li>
+                    <li><a href="#about">Tentang</a></li>
+                    <li><a href="#contact">Kontak</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <section id="home">
+                <h2>Selamat Datang</h2>
+                <p>Ini contoh paragraf HTML.</p>
+            </section>
+            <section id="about">
+                <h2>Tentang Saya</h2>
+                <p><strong>NIM:</strong> 2522500071 &#9786;</p>
+                <p><strong>Nama Lengkap:</strong> Nadia Syadahnie Putri &hearts;</p>
+                <p><strong>Tempat Lahir:</strong> Bangka Tengah &#10024;</p>
+                <p><strong>Tanggal Lahir:</strong> 20 Desember 2007 &diams;</p>
+                <p><strong>Hobby:</strong> Main game, Mendengarkan musik &#9834;</p>
+                <p><strong>Pasangan:</strong> Tidak ada &#9785;</p>
+                <p><strong>Pekerjaan:</strong> Belum Ada &#10052;</p>
+                <p><strong>Nama Orang Tua:</strong> Bapak Sarudin dan Ibu Yenti Erita</p>
+                <p><strong>Nama Kakak:</strong> Hafizh Muhammad Iqbal dan Muhammad torik</p>
+                <p><strong>Nama Adik:</strong> Moureno Erzian Isra</p> <br>
+            </section>
+            <section id="contact">
+                <h2>Kontak Saya</h2>
+                <form action="" method="GET">
+                    <label for="txtNama"><span>Nama:</span>
+                        <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama lengkap" required
+                            autocomplete="name">
+                    </label>
+                    <label for="txtEmail"><span>Email:</span>
+                        <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan alamat email" required
+                            autocomplete="email">
+                    </label>
+                    <label for="txtPesan"><span>Pesan Anda</span>
+                    <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..." required></textarea>
+                    <small id="charCount">0/200 karakter</small>
+                    </label>
+                    <button type="submit">Kirim</button>
+                    <button type="reset">Batal</button>
+                </form>
+            </section>
+        </main>
+        <footer>
+            <p>&copy; &#9786; 2025 Nadia Syadahnie Putri 2522500071</p>
+        </footer>
+        <script src="script.js"></script>
+    </body>
+
+    </html>
